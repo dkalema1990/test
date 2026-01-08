8d133250e19b2260ecbc5d04609c6af01c07812e
dkalema1990 <90250412+dkalema1990@users.noreply.github.com>
2026-01-08 20:27:54 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,16 +2,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-# Assuming you have pages/home.py and pages/dashboard.py
-st.sidebar.title("Custom Navigation")
-st.sidebar.page_link("test.py", label="Home", icon="🏠")
-## st.sidebar.page_link("pages/dashboard.py", label="Dashboard", icon="📊")
-st.sidebar.page_link("https://docs.streamlit.io", label="Streamlit Docs", icon="📚")
-
-st.title("Welcome to the Home Page")
-st.write("This is the main content area.")
-
-
 st.title("🎈 Excel Upload")
 st.write(
     "How to upload excel files in streamlit")
